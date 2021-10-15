e6f58dc17913fb6182f4bebff77401707a2794f8
i-t-m-a-n <mts04@inbox.ru>
2021-10-15 23:12:33 +0500

    create new names

--- lesson_bot/bot.py
+++ lesson_bot/bot.py
@@ -10,10 +10,13 @@ bot = tg.TeleBot(config.token_test)
 with open('lessons.json', 'r') as j:
     data = json.load(j)
 
+#======================================================================================================================#
 today = dt.today().weekday()
 yesterday = today - 1
 tomorrow = today + 1
 
+reply = bot.reply_to
+send = bot.send_message
 #======================================================================================================================#
 def get_time_table_today(cls_num, day='today'):
     for t in data[str(cls_num)]:
@@ -29,33 +32,34 @@ lesson = ''.join(data["lessons"])
 #======================================================================================================================#
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, f'Здравствуй, приятно с тобой познакомиться. Если нужна будет помощь по работе со мной, то просто напиши мне /help и я тебе все объясню')
+    reply(message, f'Здравствуй, приятно с тобой познакомиться. Если нужна будет помощь по работе со мной, то просто напиши мне /help и я тебе все объясню')
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
-    bot.reply_to(message, f'Мои команды: \n \n' + '10 - расписание 10-го класса \n' + '11 - расписание 11-го класса \n' + 'завтра 10 - посмотреть расписание 10-х на завтра \n' + 'завтра 11 - посмотреть расписание 11-х на завтра \n' + "уроки - время уроков" )
+    reply(message, f'Мои команды: \n \n' + '10 - расписание 10-го класса \n' + '11 - расписание 11-го класса \n' + 'завтра 10 - посмотреть расписание 10-х на завтра \n' + 'завтра 11 - посмотреть расписание 11-х на завтра \n' + "уроки - время уроков" )
 
 @bot.message_handler(content_types=['text'])
 def send_lessons(message):
-    if message.text.lower() == '10':
-        bot.send_message(message.chat.id, get_time_table_today(10))
-    elif message.text.lower() == 'завтра 10':
-        if today == 4: bot.reply_to(message, 'Завтра суббота')
-        elif today == 5: bot.reply_to(message, 'Завтра воскресенье')
-        else: bot.send_message(message.chat.id, get_time_table_tomorrow(10))
+    lowwer = message.text.lower()
+    if lowwer == '10':
+        send(message.chat.id, get_time_table_today(10))
+    elif lowwer == 'завтра 10':
+        if today == 4: reply(message, 'Завтра суббота')
+        elif today == 5: reply(message, 'Завтра воскресенье')
+        else: send(message.chat.id, get_time_table_tomorrow(10))
 
-    elif message.text.lower() == '11':
-        bot.send_message(message.chat.id, get_time_table_today(11))
-    elif message.text.lower() == 'завтра 11':
-        if today == 4: bot.reply_to(message, 'Завтра суббота')
-        elif today == 5: bot.reply_to(message, 'Завтра воскресенье')
-        else: bot.send_message(message.chat.id, get_time_table_tomorrow(11))
+    elif lowwer == '11':
+        send(message.chat.id, get_time_table_today(11))
+    elif lowwer == 'завтра 11':
+        if today == 4: reply(message, 'Завтра суббота')
+        elif today == 5: reply(message, 'Завтра воскресенье')
+        else: send(message.chat.id, get_time_table_tomorrow(11))
 
-    elif message.text.lower() == 'уроки':
-        bot.send_message(message.chat.id, lesson)
+    elif lowwer == 'уроки':
+        send(message.chat.id, lesson)
 
     else:
-        bot.reply_to(message, 'Error')
+        reply(message, 'Error')
 
 #======================================================================================================================#
 def telegram_polling():
